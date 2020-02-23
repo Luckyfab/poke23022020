0d5074bca4874bf2db854dd178d9c18719332b77
Luckyfab <lukasz.faber@gmail.com>
2020-02-23 11:33:21 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HelloComponent } from './hello.component';
+import { MatToolbarModule } from '@angular/material';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatToolbarModule ],
   declarations: [ AppComponent, HelloComponent ],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
